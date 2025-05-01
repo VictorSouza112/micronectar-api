36fecc5e986345d8487ecabc3866a9804c201957
Your Name <victordesouzasantos242@gmail.com>
2025-05-01 19:48:53 -0300

    Projeto base

---
+++ src/test/java/br/com/fiap/micronectar/micronectar_api/MicronectarApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.micronectar.micronectar_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicronectarApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
